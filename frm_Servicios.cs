f93438cf15aa6d6db362d311e5648a058fad5e7c
Francis Gonzalez <Kitomc.rd@gmail.com>
2019-08-19 22:56:37 -0400

    Agregar archivos de proyecto.

---
+++ frm_Servicios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Papeleria_Patio_Colombia
+{
+    public partial class frm_Servicios : Form
+    {
+        public frm_Servicios()
+        {
+            InitializeComponent();
+        }
+    }
+}
